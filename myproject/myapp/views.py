cde1d6a1fad74156a7759ae7977ca0e8863a137c
titen100 <titen100@gmail.com>
2025-02-25 16:00:57 +0100

    django now functional

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
 def home(request):
-    return HttpResponse("Hello DJango")
+    return HttpResponse("Welcome to MyApp Home Page!")  # ✅ Simple response for testing
