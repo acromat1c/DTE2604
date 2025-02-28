268a346e8dddf1827c09d0f20adc1015de1cb1d8
acromat1c <i-reseland@outlook.com>
2025-02-28 16:33:58 +0100

    added html templates

--- django app/programing_course/programing_course_app/urls.py
+++ django app/programing_course/programing_course_app/urls.py
@@ -4,5 +4,17 @@ from . import views
 
 app_name = "programing_course_app"
 urlpatterns = [
-    path("",views.index, name="index"),
+    path("index",views.index, name="index"),
+    path("",views.main, name="main"),
+    path("login",views.login, name="login"),
+    path("signup",views.signup, name="signup"),
+    path("user",views.user, name="user"),
+    path("user/settings",views.userSettings, name="userSettings"),
+    path("groups",views.groupList, name="groupList"),
+    path("groups/<str:name>",views.group, name="group"),
+    path("friends/<str:name>",views.friend, name="friend"),
+    path("friends",views.friendList, name="friendList"),
+    path("subjects",views.subjectList, name="subjectList"),
+    path("subjects/<str:namesub>",views.subject, name="subject"),
+    path("subjects/<str:namesub>/<str:name>",views.mission, name="mission"),
     ]
--- django app/programing_course/programing_course_app/views.py
+++ django app/programing_course/programing_course_app/views.py
@@ -3,3 +3,51 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "course/index.html")
+
+
+
+def login(request):
+    return render(request, "course/login.html")
+
+def signup(request):
+    return render(request, "course/signup.html")
+
+
+
+def main(request):
+    return render(request, "course/main.html")
+
+
+
+def user(request):
+    return render(request, "course/user.html")
+
+def userSettings(request):
+    return render(request, "course/userSettings.html")
+
+
+
+def friendList(request):
+    return render(request, "course/friendList.html")
+
+def friend(request,name):
+    return render(request, "course/friend.html", {"name": name})
+
+
+
+def groupList(request):
+    return render(request, "course/groupList.html")
+
+def group(request,name):
+    return render(request, "course/group.html", {"name": name})
+
+
+
+def subjectList(request):
+    return render(request, "course/subjectList.html")
+
+def subject(request,namesub):
+    return render(request, "course/subject.html", {"namesub": namesub})
+
+def mission(request,namesub,name):
+    return render(request, "course/mission.html", {"namesub": namesub, "name": name})
