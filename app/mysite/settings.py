440456fa830d7b6d4baea7f228c786a5a09cb94e
Mathias <mathias.e.karlsen@gmail.com>
2025-03-13 12:12:39 +0100

    added support for MariaDB

--- app/mysite/settings.py
+++ app/mysite/settings.py
@@ -74,12 +74,26 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# Sqlite3 database
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+# MariaDB database
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'mydb',
+#         'USER': 'user',
+#         'PASSWORD': 'password',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306'
+#     }
+# }
 
 
 # Password validation
