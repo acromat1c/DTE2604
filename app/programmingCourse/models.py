429cf15766cd45d33fa5fca1cfb86280508eaa3d
acromat1c <i-reseland@outlook.com>
2025-03-19 15:43:15 +0100

    Add files via upload

---
+++ app/programmingCourse/models.py
@@ -0,0 +1,130 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+
+
+class Friend(models.Model):
+    user1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name="friend_user1")
+    user2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name="friend_user2")
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['user1', 'user2'], name='unique_friends'),
+            models.CheckConstraint(
+                check=models.Q(user1__lt=models.F('user2')),
+                name='user1_smaller_than_user2'
+            ),
+        ]
+
+class FriendRequest(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="friend_requests_sent")
+    recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name="friend_requests_received")
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['sender', 'recipient'], name='unique_friend_request')
+        ]
+
+class Achievement(models.Model):
+    name = models.CharField(max_length=45)
+    description = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.name
+
+class UserAchievement(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    achievement = models.ForeignKey(Achievement, on_delete=models.CASCADE)
+    time = models.DateField()
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['user', 'achievement'], name='unique_user_achievement')
+        ]
+
+class Group(models.Model):
+    groupOwner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="owned_groups")
+    groupName = models.CharField(max_length=45)
+
+    def __str__(self):
+        return self.groupName
+
+class GroupMember(models.Model):
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['group', 'user'], name='unique_group_member')
+        ]
+
+class GroupMessage(models.Model):
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.CharField(max_length=1000)
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+    text = models.TextField()
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['name'], name='unique_course')
+        ]
+
+    def __str__(self):
+        return self.name
+
+class Module(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+    text = models.TextField()
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['name', 'course'], name='unique_module')
+        ]
+
+    def __str__(self):
+        return f"{self.name} | {self.course}"
+
+class Mission(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+    text = models.TextField()
+    answer = models.CharField(max_length=1000)
+    maxPoints = models.IntegerField()
+    module = models.ForeignKey(Module, on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['name', 'module'], name='unique_Mission')
+        ]
+
+    def __str__(self):
+        return f"{self.name} | {self.module}"
+
+class MissionCompleted(models.Model):
+    mission = models.ForeignKey(Mission, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    points = models.IntegerField()
+    completionTime = models.DateField()
+    answer = models.CharField(max_length=1000)
+
+    class Meta:
+            constraints = [
+                models.UniqueConstraint(fields=['user', 'mission'], name='unique_complete')
+            ]
+
+    def __str__(self):
+        return f"{self.user} | {self.mission}"
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='profile_pics/', default='default.jpg')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
---
+++ app/programmingCourse/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+
+from . import views
+
+app_name = "programing_course_app"
+urlpatterns = [
+    path("index",views.index, name="index"),
+    path("",views.main, name="main"),
+    path("login",views.login, name="login"),
+    path("signup",views.signup, name="signup"),
+    path("logout",views.logout, name="logout"),
+    path("user",views.user, name="user"),
+    path("user/settings",views.userSettings, name="userSettings"),
+    path("user/<str:username>/", views.user_page, name="userPage"),
+    path("groups",views.groupList, name="groupList"),
+    path("groups/<str:name>",views.group, name="group"),
+    path("friends/<str:name>",views.friend, name="friend"),
+    path("friends",views.friendList, name="friendList"),
+    path("overview",views.overview, name="overview"),
+    path("overview/<str:nameCourse>",views.course, name="course"),
+    path("overview/<str:nameCourse>/<str:nameModule>",views.module, name="module"),
+    path("overview/<str:nameCourse>/<str:nameModule>/<str:nameMission>",views.mission, name="mission"),
+    path("test",views.test, name="test"),
+    ]
