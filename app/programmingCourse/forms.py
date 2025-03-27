500bdf3e6e015b5b6bb8abea8ee97b0557b8e910
Peps2k <karlvjonsson@gmail.com>
2025-03-27 21:09:21 +0100

    Create forms for group creation and settings management

--- app/programmingCourse/forms.py
+++ app/programmingCourse/forms.py
@@ -20,3 +20,14 @@ class GroupCreateForm(forms.ModelForm):
             'image': forms.FileInput(attrs={'class': 'form-control'}),
             'is_private': forms.CheckboxInput(),
         }
+
+class GroupEditForm(forms.ModelForm):
+    class Meta:
+        model = Group
+        fields = ['groupName', 'description', 'image', 'is_private']
+        widgets = {
+            'groupName': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 4}),
+            'image': forms.FileInput(attrs={'class': 'form-control'}),
+            'is_private': forms.CheckboxInput(),
+        }
