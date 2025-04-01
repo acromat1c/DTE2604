3028b17fac7bf7c63cd6e9f319a29e3d58d11419
acromat1c <i-reseland@outlook.com>
2025-04-01 07:36:01 +0200

    Update models.py

--- app/programmingCourse/models.py
+++ app/programmingCourse/models.py
@@ -134,14 +134,14 @@ class MissionCompleted(models.Model):
             ]
 
     def __str__(self):
-        return f"{self.user.username} | {self.mission}"
+        return f"{self.user} | {self.mission}"
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='profile_pics/', default='default.jpg')
 
     def __str__(self):
-        return f'{self.user.username} Profile'
+        return f'{self.user} Profile'
 
 class UserBalance(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
